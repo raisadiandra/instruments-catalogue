505c66d6094ee5bffc565b618874393aed715224
Raisa Diandra Survijanto <raisa.diandra@yahoo.com>
2023-09-11 21:44:24 +0700

    added tables of datas in models

--- main/views.py
+++ main/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
+from .models import Product
 
 def show_main(request):
+    product = Product(
+        name='Piano',
+        amount=10,
+        description='The piano is a keyboard instrument that produces sound when pressed on the keys.',
+        price=10000000)
+    
+    product.save()
+
     context = {
         'name': 'Raisa Diandra Survijanto',
-        'class': 'PBP E'
+        'class': 'PBP E',
+        'product': product,
     }
 
     return render(request, "main.html", context)
