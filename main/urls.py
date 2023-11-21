b19156587bc47105b3fce1810de75dede9992f57
Raisa Diandra Survijanto <raisa.diandra@yahoo.com>
2023-11-21 23:19:17 +0700

    tugas 9 done

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from main.views import show_main, create_item, show_xml, show_json, show_xml_by_id, show_json_by_id, register, login_user, logout_user, edit_item, delete_item, get_item_json, create_ajax
+from main.views import show_main, create_item, show_xml, show_json, show_xml_by_id, show_json_by_id, register, login_user, logout_user, edit_item, delete_item, get_item_json, create_ajax, create_product_flutter
 app_name = 'main'
 
 urlpatterns = [
@@ -15,5 +15,6 @@ urlpatterns = [
     path('edit-item/<int:id>', edit_item, name='edit_item'),
     path('delete/<int:id>', delete_item, name='delete_item'),
     path('get-item/', get_item_json, name='get_item_json'),
-    path('create-ajax/', create_ajax, name='create_ajax')
+    path('create-ajax/', create_ajax, name='create_ajax'),
+    path('create-flutter/', create_product_flutter, name='create_product_flutter'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,6 +1,7 @@
+import json
 from django.shortcuts import render
 from .models import Item
-from django.http import HttpResponseNotFound, HttpResponseRedirect
+from django.http import HttpResponseNotFound, HttpResponseRedirect, JsonResponse
 from main.forms import ItemForm
 from django.urls import reverse
 from django.http import HttpResponse
@@ -126,3 +127,23 @@ def create_ajax(request):
         return HttpResponse(b"CREATED", status=201)
 
     return HttpResponseNotFound()
+
+@csrf_exempt
+def create_product_flutter(request):
+    if request.method == 'POST':
+        
+        data = json.loads(request.body)
+
+        new_product = Item.objects.create(
+            user = request.user,
+            name = data["name"],
+            amount = data["amount"],
+            price = int(data["price"]),
+            description = data["description"]
+        )
+
+        new_product.save()
+
+        return JsonResponse({"status": "success"}, status=200)
+    else:
+        return JsonResponse({"status": "error"}, status=401)
